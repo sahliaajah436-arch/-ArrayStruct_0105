bbbd72df26564b8a78bb40af3218cc867723160b
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-07 18:38:34 +0700

    setup program dasar

--- struct.cpp
+++ struct.cpp
@@ -1,27 +1,3 @@
 #include <iostream>
 using namespace std;
 
-struct Orang
-{
-    string nama;
-    string alamat;
-    int umur;
-};
-
-int main()
-{
-    //Deklarasi variabel struct
-    Orang mhs;
-    //Mengisi data
-    cout << "Isikan data nama : ";
-    cin >> mhs.nama;
-    cout << "Isikan data alamat : ";
-    cin >> mhs.alamat;
-    cout << "Isikan data umur : ";
-    cin >> mhs.umur;
-    cout << endl;
-
-     cout << "Data Tampil" << endl;
-    cout << "Nama : " << mhs.nama << endl;
-    cout << "Alamat : " << mhs.alamat << endl;
-    cout << "Umur : " << mhs.umur << endl; 
