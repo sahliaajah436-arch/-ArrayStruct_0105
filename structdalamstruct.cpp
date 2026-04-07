f7a2cc592649ae758349cfd9f11db970ecb970de
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-07 19:12:20 +0700

    menambahkan input data (nama, desa, kota, umur)

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -18,5 +18,17 @@ int main()
 {
     //Deklarasi variable struct
     Orang mhs;
+    // Mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
+
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
 
 }
